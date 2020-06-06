43fe3858db9b3336596544989b81ba03c8cac3d3
Alexandre MONSCHEIN <a.monschein@lyon.ort.asso.fr>
2020-06-06 17:28:58 +0200

    commit the vuejs project folder

---
+++ equitrec/www/src/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <p>test</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: function() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
