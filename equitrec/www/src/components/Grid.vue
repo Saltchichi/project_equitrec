87c75dc9b9a1f5154f6b867e5f5ee11230e32781
Antoine MARTIN <a.martin@lyon.ort.asso.fr>
2020-06-19 09:15:18 +0200

    adding grid file begin

---
+++ equitrec/www/src/components/Grid.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <h1>WELCOME</h1>
+    
+  </div>
+</template>
+<script>
+import { writeFileSync, readFileSync } from 'fs';
+import fs from "fs";
+export default {
+  name: "Grid",
+  data: function() {
+    return {};
+  },
+  mounted: function() {
+      this.convertJson()
+  },
+  methods: {
+    convertJson() {
+      let fs = require("fs");
+      fs.readFile(
+        "../src/json_files/branches_basses_en_main.json",
+        "utf8",
+        (err, jsonString) => {
+          if (err) {
+            console.log("Erreur lors de la lecture du fichier :", err);
+            return;
+          }
+          try {
+            let epreuve = JSON.parse(jsonString);
+            console.log("nom de l'epreuve:", epreuve.epreuve);
+          } catch (err) {
+            console.log("Erreur lors de l'analyse de la chaîne JSON:", err);
+          }
+        }
+      );
+    }
+  }
+};
+</script>
